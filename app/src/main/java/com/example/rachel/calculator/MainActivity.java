174318f786ee5fe92bba0a8eef745945428c5e50
racheland <younghe_lim@hotmail.com>
2019-01-19 13:32:46 +0900

    - 계산기 UI 구성 - MainActivity 작성

--- app/src/main/java/com/example/rachel/calculator/MainActivity.java
+++ app/src/main/java/com/example/rachel/calculator/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.rachel.calculator;
 
+import android.app.Activity;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView resultView;
+    private Button buttonOne;
+
+    private String result = "";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultView = findViewById(R.id.textv_result);
+        buttonOne = findViewById(R.id.btn_1);
+
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result += "1";
+                resultView.setText(result);
+            }
+        });
     }
 }
